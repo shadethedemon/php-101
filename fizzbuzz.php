3f3416c8070f6111e2042396c3513de5461ed6d9
chadicus <chadwickgray@gmail.com>
2015-08-17 15:41:18 -0400

    Fix my spelling

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,7 +3,7 @@
 $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
 
 /**
- If $input is divisable by 3 output 'Fizz'
- If $input is divisable by 5 output 'Buzz'
- If $input is divisable by both 3 and 5 output 'FizzBuzz'
+ If $input is divisible by 3 output 'Fizz'
+ If $input is divisible by 5 output 'Buzz'
+ If $input is divisible by both 3 and 5 output 'FizzBuzz'
 */

c56d13f2f38d8e481bdc128717c127108bdfe5fa
chadicus <chadwickgray@gmail.com>
2015-08-17 12:52:52 -0400

    add directions

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,2 +1,9 @@
 <?php
+
 $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
+
+/**
+ If $input is divisable by 3 output 'Fizz'
+ If $input is divisable by 5 output 'Buzz'
+ If $input is divisable by both 3 and 5 output 'FizzBuzz'
+*/

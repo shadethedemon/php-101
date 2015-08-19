ba046904d9fa41784a50fa6e65574cffc056eefd
Daniel Burgner <daniel.burgner@dominionenterprises.com>
2015-08-19 13:20:31 -0400

    Fizzbuzz solution - Daniel.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ fizzbuzz.php
+ Daniel Burgner PHP 101
+ August 19, 2015
+ */
 
 $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
 
@@ -7,3 +12,11 @@ $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
  If $input is divisable by 5 output 'Buzz'
  If $input is divisable by both 3 and 5 output 'FizzBuzz'
 */
+if (($input % 3) == 0) {
+	print 'Fizz';
+}
+if (($input % 5) == 0) {
+	print 'Buzz';
+}
+print PHP_EOL;
+

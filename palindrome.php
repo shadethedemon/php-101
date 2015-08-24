1bc9e9f321de6ba2c1a096c15562921e4d13317e
chadicus <chadwickgray@gmail.com>
2015-08-24 11:40:19 -0400

    Add exercise 2

---
+++ palindrome.php
@@ -0,0 +1,21 @@
+<?php
+$input = isset($argv[1]) ? $argv[1] : die("No input given\n");
+
+/**
+ * Exercise 2 Palindrome
+ *
+ * A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. For
+ * this exercise you will write a script that will determine if the given input is a palindrome.
+ *
+ * Output "Yes, '<input>' is a palindrome!" if the given input is a palindrome, otherwise output "No, '<input>' is not a palindrome."
+ *
+ * Examples:
+ * Input: 'Radar'
+ * Output: Yes, 'Radar' is a palindrome!
+ *
+ * Input: 'FOO'
+ * Output: No, 'FOO' is not a palindrome.
+ *
+ * Input: '10101'
+ * Output: Yes, '10101' is a palindrome!
+ */

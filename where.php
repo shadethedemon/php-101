7ec4753e16c03b39446476575e6e488f85cd5afb
Reese Howell <reese.howell@dominionenterprises.com>
2015-09-18 16:21:58 -0400

    temp

--- where.php
+++ where.php
@@ -9,4 +9,26 @@ $books = require_once __DIR__ . '/books.php';
  * has the value of $value. Echo the id and title of each book that matches the given criteria.
  */
 
+$list = [];
 
+$time =  970372800;
+echo jdtojulian(unixtojd($time)), PHP_EOL;
+exit;
+
+
+foreach ($books as $book) {
+    foreach ($book as $k => $v) {
+        $pos = strpos(strtolower($v), strtolower($value));
+        if ($k == $index && $pos !== false) {
+            $list[] = $book['id']; 
+        }
+        if ($index == 'publishDate') {
+//            echo jdtojulian(970372800);
+        } 
+    }
+}
+
+foreach($list as $hit) {
+    echo $hit . PHP_EOL;
+    echo $books[$hit]['author'] . PHP_EOL;
+}

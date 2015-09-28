bebe0938bd96f3f00b0af947bf1debf7225f706b
chadicus <chadwickgray@gmail.com>
2015-09-28 11:17:47 -0400

    Add notes on dateTime object

--- objects.php
+++ objects.php
@@ -5,19 +5,49 @@
 //DateTime
 
 //construct
+$dateTime = new \DateTime(); //datetime of NOW using local TZ
+
+//echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
+
+$now = time();
+
+//echo "Current Timestamp: {$now}\n";
+
+$dateTime = new \DateTime("@{$now}");
+//echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
+
+$dateTime = new \DateTime('+1');
+//echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
 
 //compare
 
+$thisDate = new \DateTime('-3 days');
+$thatDate = new \DateTime('-2 days');
+
+//var_dump($thisDate === $thatDate);
+//var_dump($thisDate > $thatDate);
+//var_dump($thisDate < $thatDate);
+
+$now = time();
+
+$thisDateTime = new \DateTime("@{$now}");
+$thatDateTime = new \DateTime("@{$now}");
+
+//var_dump($thisDateTime === $thatDateTime);
+
 //add time
+$dateTime = new \DateTime();
 
-//DateTimeZone
+//add 3 days
+$dateTime->add(\DateInterval::createFromDateString('3 days'));
+//echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
 
-//construct
+$tz = new \DateTimeZone('Pacific/Honolulu');
+$dateTime = new \DateTime('now', $tz);
+//echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
 
-//Directory
+$dateTime = new \DateTime('now', new \DateTimeZone('America/New_York'));
 
-//construct
+$dateTime->setTimeZone(new \DateTimeZone('Pacific/Honolulu'));
 
-//iterate
-
-
+echo $dateTime->format('Y-m-d H:i:s T') . PHP_EOL;
